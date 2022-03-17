e49496fbedb59e17c45d2b04850b5aa050076ee5
stud-402 <stud-402@PC-402-11>
2022-03-17 13:46:41 +0700

    Добавьте файлы проекта.

---
+++ CarRent/CarRent/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using CarRent.ViewModels;
+using CarRent.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace CarRent
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ CarRent/CarRent/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using CarRent.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace CarRent.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
